f9846a8ff8dfd89de6cf85dd8030fa1305bf955e
Jennifer Beem <jenniferbeem@gmail.com>
2017-04-07 09:29:29 -0700

    add member model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Hiking Group';
+  members: Member[] = [
+   new Member("Jamie", 24,
+       "general", 3, 1),
+   new Member("John", 32,
+       "general", 1, 2),
+   new Member("Moose", 9,
+       "expert", 6, 3)
+ ];
 }
---
+++ src/app/member.model.ts
@@ -0,0 +1,3 @@
+export class Album {
+  constructor (public name: string, public age: number, public type: string, public years: number, public id: number) { }
+}
